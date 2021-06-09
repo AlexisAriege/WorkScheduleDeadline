023a4e1574da0b75874f4f0ee88851a225ef34bf
AlexisAriege <alexis.generes@gmail.com>
2021-06-09 11:54:33 +0200

    Measuring Instructions and cycles on all cpu

--- measure_all_ipc.py
+++ measure_all_ipc.py
@@ -19,8 +19,8 @@ code="""
 #include <linux/sched.h>
 
 //associé
+BPF_PERF_ARRAY(cpu_instructions, 12);
 BPF_PERF_ARRAY(cpu_cycles, 12);
-
 
 
 struct data {
@@ -28,6 +28,7 @@ struct data {
     char comm[TASK_COMM_LEN];
     u64 ts;
     u64 cycle;
+    u64 instruction;
     u32 id_cpu;
 };
 
@@ -41,11 +42,17 @@ int counter_ipc(struct bpf_perf_event_data *ctx) {
     bpf_get_current_comm(&s.comm, sizeof(s.comm));
 
     int cpu = bpf_get_smp_processor_id();
-    int value = cpu_cycles.perf_read(cpu);
-    if ( value<0 ){
+    u64 value = cpu_cycles.perf_read(cpu);
+    if ( (s64)value<0 ){
         s.cycle=0;
     }
     else s.cycle=value;
+
+    u64 value_instructions = cpu_instructions.perf_read(cpu);
+    if ( (s64)value_instructions<0 ){
+        s.instruction=0;
+    }
+    else s.instruction=value_instructions;
     s.id_cpu = cpu;
     events.perf_submit(ctx, &s, sizeof(s));
     return 0;
@@ -57,13 +64,18 @@ b = BPF(text=code)
 try:
     b.attach_perf_event(
         ev_type=PerfType.HARDWARE, ev_config=PerfHWConfig.INSTRUCTIONS,
-        fn_name="counter_ipc",cpu=2, sample_period=1000000)
+        fn_name="counter_ipc",sample_period=10000000) #, sample_freq=1
+
+
 
 except Exception:
     print("Failed to attach to a hardware event. Is this a virtual machine?")
     exit()
 PERF_TYPE_RAW = 4
 b["cpu_cycles"].open_perf_event(PERF_TYPE_RAW, 0x0000003C)
+#Instruction
+b["cpu_instructions"].open_perf_event(PERF_TYPE_RAW, 0x000000C0)
+#b["cpu_cycles"].open_perf_event(b["cpu_cycles"].HW_CPU_CYCLES)
 start = 0
 def print_event(cpu, data, size):
     event = b["events"].event(data)
@@ -71,7 +83,7 @@ def print_event(cpu, data, size):
     if start == 0:
             start = event.ts
     time_s = (float(event.ts - start)) / 1000000000
-    print("t=%6.5f ,cpu=%d, valeur IPC=%d" % (time_s,event.id_cpu ,event.cycle))
+    print("t=%6.5f ,cpu=%d, valeur cycle=%d, instruction=%d" % (time_s,event.id_cpu ,event.cycle, event.instruction))
 b["events"].open_perf_buffer(print_event)
 while True:
 	try:
